53a4b843ab060502c039d9aac2edfea86acc3d36
idonkoh-halm <isaac.donkoh-halm@innovationcharter.org>
2015-11-23 06:29:44 -0500

    Bunch of changes. fixing win

--- KeyboardTurtle.py
+++ KeyboardTurtle.py
@@ -1,35 +1,27 @@
 #Original Code  from http://openbookproject.net/thinkcs/python/english3e/events.html
 import turtle
 
-turtle.setup(400,500)                # Determine the window size
-wn = turtle.Screen()                 # Get a reference to the window
-wn.title("Handling keypresses!")     # Change the window title
-wn.bgcolor("darkgoldenrod")             # Set the background color
-tess = turtle.Turtle()               # Create our favorite turtle
 
-# The next four functions are our "event handlers".
+wn = turtle.Screen()
+wn.title("Handling keypresses!")
+wn.bgcolor("darkgoldenrod")
+oliver = turtle.Turtle()
+
+
 def h1():
-    tess.setheading(60)
+    oliver.setheading(60)
 
 def h2():
-    tess.circle(45,5)
-
+    oliver.circle(45,5)
 def h3():
-   tess.setheading(-60)
-def h5():
-    tess.circle(45,5)
-   
+   oliver.setheading(-60)
 def h4():
-    wn.bye()                        # Close down the turtle window
+    oliver.circle(45,5)
 
-# These lines "wire up" keypresses to the handlers we've defined.
 wn.onkey(h1, "Up")
 wn.onkey(h2, "Left")
 wn.onkey(h3, "Down")
-wn.onkey(h4, "q")
-wn.onkey(h5, "right")
+wn.onkey(h4, "right")
 
-# Now we need to tell the window to start listening for events,
-# If any of the keys that we're monitoring is pressed, its
-# handler will be called.
+
 wn.listen()
