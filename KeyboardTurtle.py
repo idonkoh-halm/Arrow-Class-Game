04610325b773825898be5e477288f28a7bfd1d9c
idonkoh-halm <isaac.donkoh-halm@innovationcharter.org>
2015-11-23 13:24:53 -0500

    Now, there is color

--- KeyboardTurtle.py
+++ KeyboardTurtle.py
@@ -1,6 +1,6 @@
 #Original Code  from http://openbookproject.net/thinkcs/python/english3e/events.html
 import turtle
-
+import random
 
 wn = turtle.Screen()
 oliver = turtle.Turtle()
@@ -15,15 +15,15 @@ def h3():
     oliver.setheading(-60)
 def h4():
     oliver.circle(45,5)
-def screenClicked():
-    oliver.color(red)
+def screenClicked(x,y):
+    a = ["Red","Blue","Green", "Orange"]
+    colorvalue=random.randint(0,3)
+    oliver.color(a[colorvalue])
+def fire():
+    oliver.forward(50)
 wn.onkey(h1, "Up")
 wn.onkey(h2, "Left")
 wn.onkey(h3, "Down")
 wn.onkey(h4, "Right")
-wn.onclick(screenClicked)
-wn.onrelease(fire)
-
-
-        
+wn.onclick(screenClicked)        
 wn.listen()
