87b2c9bb0364784825921de34b1ac8055886414a
idonkoh-halm <isaac.donkoh-halm@innovationcharter.org>
2015-11-25 10:41:00 -0500

    Whole bunch of changes

--- KeyboardTurtle.py
+++ KeyboardTurtle.py
@@ -2,9 +2,15 @@
 import turtle
 import random
 
+class Arrow (turtle.Turtle):
+    def ___init___ (self, xvel=300):
+        turtle.Turtle.___init___(self)
+        self.tracer(0)
+    def move(self):
+        pass
+
 wn = turtle.Screen()
 oliver = turtle.Turtle()
-
 
 def h1():
     oliver.setheading(60)
@@ -15,9 +21,16 @@ def h3():
     oliver.setheading(-60)
 def h4():
     oliver.circle(45,5)
+def Return_to_Origin():
+    oliver.up()
+    oliver.goto(0,0)
+    oliver.down()
+    b = ["Red", "Blue", "Green"]
+    colorvalue=random.randint(0,2)
+    oliver.color(b[colorvalue])
 def screenClicked(x,y):
-    a = ["Red","Blue","Green", "Orange"]
-    colorvalue=random.randint(0,3)
+    a = ["Red","Blue","Green", "Medium Blue", "Lime Green", "DodgerBlue", "Dark Blue"]
+    colorvalue=random.randint(0,6)
     oliver.color(a[colorvalue])
 def fire():
     oliver.forward(50)
@@ -25,5 +38,7 @@ wn.onkey(h1, "Up")
 wn.onkey(h2, "Left")
 wn.onkey(h3, "Down")
 wn.onkey(h4, "Right")
+wn.onkey(fire, "f")
+wn.onkey(Return_to_Origin, "0")
 wn.onclick(screenClicked)        
 wn.listen()
