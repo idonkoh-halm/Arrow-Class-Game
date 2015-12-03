e08380fe398627d80aa61bf1b4bb981a137e4d1c
idonkoh-halm <isaac.donkoh-halm@innovationcharter.org>
2015-12-03 15:49:10 -0500

    new extra help

---
+++ extrahelp.py
@@ -0,0 +1,11 @@
+class Arrow_S2:
+    def __init__(self):
+        self.protag = "Queen"
+        self.antag = "Slade"
+        self.deutrag = "Diggle"
+        self.tritag ="Smoak"
+    def island(self, character):
+        self.character=character
+    
+
+Arrow_S2()
