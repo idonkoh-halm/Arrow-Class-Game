fbe59ee3cd73111a9df2975a2ad3322506d61aca
idonkoh-halm <isaac.donkoh-halm@innovationcharter.org>
2015-12-17 16:02:40 -0500

    Extrahelp, class

--- KeyboardTurtle.py
+++ KeyboardTurtle.py
@@ -1,4 +1,3 @@
-#Original Code  from http://openbookproject.net/thinkcs/python/english3e/events.html
 import turtle
 import random
 
@@ -6,21 +5,32 @@ class Arrow (turtle.Turtle):
     def ___init___ (self, xvel=300):
         turtle.Turtle.___init___(self)
         self.tracer(0)
+
+    def reset_direction (self):
+        self.setheading(60)
+    def thrusters(self):
+        self.forward(50)
     def move(self):
         pass
+oliver = Arrow()
+
+laura = Arrow()
 
 wn = turtle.Screen()
-oliver = turtle.Turtle()
+#oliver = turtle.Turtle()
 
 def h1():
-    oliver.setheading(60)
+    oliver.setheading(50)
 
 def h2():
     oliver.circle(45,5)
+
 def h3():
     oliver.setheading(-60)
+
 def h4():
-    oliver.circle(45,5)
+    oliver.circle(-45,5)
+
 def Return_to_Origin():
     oliver.up()
     oliver.goto(0,0)
@@ -28,17 +38,23 @@ def Return_to_Origin():
     b = ["Red", "Blue", "Green"]
     colorvalue=random.randint(0,2)
     oliver.color(b[colorvalue])
+
 def screenClicked(x,y):
     a = ["Red","Blue","Green", "Medium Blue", "Lime Green", "DodgerBlue", "Dark Blue"]
+    b= ["Orange", "C", "aqua", "gold", "darkkhaki", "royalblue", "peru", "magneta"]
     colorvalue=random.randint(0,6)
     oliver.color(a[colorvalue])
-def fire():
-    oliver.forward(50)
+    laura.color(b[colorvalue])
+
+
+wn.onkey(oliver.reset_direction, 'Up')
+wn.onkey(laura.reset_direction,'a')
 wn.onkey(h1, "Up")
 wn.onkey(h2, "Left")
 wn.onkey(h3, "Down")
 wn.onkey(h4, "Right")
-wn.onkey(fire, "f")
+wn.onkey(oliver.thrusters, "Up")
+wn.onkey(laura.thrusters, "f")
 wn.onkey(Return_to_Origin, "0")
 wn.onclick(screenClicked)        
 wn.listen()
